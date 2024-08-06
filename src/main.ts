b26483cd2e43d0883ba83db78e0274eff1e6882c
Patrick Barton Grace <132808103+Tuthar@users.noreply.github.com>
2024-08-06 15:57:10 +1000

    Fixed build errors

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import './style.css'
+// @ts-ignore
 import App from './App.vue'
 // Vuetify
 import 'vuetify/styles'
--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,6 @@
 import { stationNames_v1, stationNames_v2 } from "./stations";
 import shuffle from "fisher-yates";
-
+// @ts-ignore
 import pseudoRandom from "pseudo-random";
 
 // precalculate target stations, avoiding short repeats
